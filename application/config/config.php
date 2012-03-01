60d0f75ad7e693b45bbcd5f6f14bfeb93e557f1c
Oscar Dias <oscardias@gmail.com>
2012-03-01 12:45:26 -0300

    Remove index.php from urls

--- application/config/config.php
+++ application/config/config.php
@@ -26,7 +26,7 @@ $config['base_url']	= 'http://localhost/task_board/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
