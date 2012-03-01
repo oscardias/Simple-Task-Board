4410e9d58d90f20a6a15eeb0121fe254296e2a28
Oscar Dias <oscardias@gmail.com>
2012-03-01 12:43:59 -0300

    Install main user

---
+++ application/controllers/install.php
@@ -0,0 +1,49 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Install extends CI_Controller {
+
+    public function index()
+    {
+        $data = array();
+        
+        // Check if users are already there
+        $this->load->model('user_model');
+        $users = $this->user_model->get();
+        if($users){
+            $data['already_installed'] = true;
+        } else {
+            $data['already_installed'] = false;
+        }
+        
+        // Load View
+        $data['page_title']  = "Installation";
+        
+        $data['email'] = '';
+        $data['password'] = '';
+        
+        $this->template->show('login', $data);
+    }
+    
+    public function run()
+    {
+        // Check if users are already there
+        $this->load->model('user_model');
+        $users = $this->user_model->get();
+        if(!$users){
+            $insert = array(
+                'email' => $this->input->post('email'),
+                'password' => $this->input->post('password')
+            );
+            $this->user_model->create($insert);
+        }
+        
+        // Load View
+        $data['page_title']  = "Login";
+        
+        $data['email'] = '';
+        $data['password'] = '';
+        
+        $this->template->show('login', $data);
+    }
+    
+}
