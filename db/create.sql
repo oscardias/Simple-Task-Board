5bc93d46817b1e7fb0ed8ee9bf261fdab449437a
Oscar Dias <oscardias@gmail.com>
2012-03-01 17:33:58 -0300

    New database for user handling

--- db/create.sql
+++ db/create.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tempo de Geração: 28/02/2012 às 23h23min
+-- Tempo de Geração: 01/03/2012 às 20h32min
 -- Versão do Servidor: 5.5.20
 -- Versão do PHP: 5.3.9
 
@@ -23,20 +23,87 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `ci_sessions`
+--
+
+DROP TABLE IF EXISTS `ci_sessions`;
+CREATE TABLE IF NOT EXISTS `ci_sessions` (
+  `session_id` varchar(40) NOT NULL DEFAULT '0',
+  `ip_address` varchar(16) NOT NULL DEFAULT '0',
+  `user_agent` varchar(50) NOT NULL,
+  `last_activity` int(10) unsigned NOT NULL DEFAULT '0',
+  `user_data` text NOT NULL,
+  PRIMARY KEY (`session_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `project`
+--
+
+DROP TABLE IF EXISTS `project`;
+CREATE TABLE IF NOT EXISTS `project` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user` int(10) unsigned NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `task`
 --
 
+DROP TABLE IF EXISTS `task`;
 CREATE TABLE IF NOT EXISTS `task` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `project` int(10) unsigned NOT NULL,
+  `id` int(10) unsigned NOT NULL,
+  `user` int(10) unsigned NOT NULL,
   `status` tinyint(4) unsigned NOT NULL,
   `title` varchar(50) NOT NULL,
   `priority` tinyint(4) unsigned NOT NULL,
   `text` text NOT NULL,
   `files` text NOT NULL,
   `database` text NOT NULL,
-  PRIMARY KEY (`id`),
+  PRIMARY KEY (`project`,`id`),
   KEY `position` (`status`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `task_comments`
+--
+
+DROP TABLE IF EXISTS `task_comments`;
+CREATE TABLE IF NOT EXISTS `task_comments` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `task` int(10) unsigned NOT NULL,
+  `user` int(10) unsigned NOT NULL,
+  `comment` text NOT NULL,
+  `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `task_history`
+--
+
+DROP TABLE IF EXISTS `task_history`;
+CREATE TABLE IF NOT EXISTS `task_history` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `task` int(10) unsigned NOT NULL,
+  `status` tinyint(3) unsigned NOT NULL,
+  `user` int(10) unsigned NOT NULL,
+  `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -44,6 +111,7 @@ CREATE TABLE IF NOT EXISTS `task` (
 -- Estrutura da tabela `transport`
 --
 
+DROP TABLE IF EXISTS `transport`;
 CREATE TABLE IF NOT EXISTS `transport` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `open` tinyint(1) NOT NULL DEFAULT '1',
@@ -52,7 +120,23 @@ CREATE TABLE IF NOT EXISTS `transport` (
   `database` text NOT NULL,
   `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(40) NOT NULL,
+  `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
