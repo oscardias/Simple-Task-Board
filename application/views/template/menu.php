497673c055bdea5d2122da8a2c023f83eb2e9549
Oscar Dias <oscardias@gmail.com>
2012-03-04 17:15:23 -0300

    Menu template

---
+++ application/views/template/menu.php
@@ -0,0 +1,14 @@
+<div class="home-title blue-gradient">Simple Task Board</div>
+<div id="menu">
+    <?php if($view == 'dashboard') { ?>
+        <!-- Dashboard menu -->
+        <?php echo anchor('user', 'Edit Userts', 'class="btn btn_dashboard"'); ?>
+    <?php } elseif($view == 'task_board') { ?>
+        <!-- Task board menu -->
+        <?php echo anchor('dashboard', 'Dashboard', 'class="btn btn_dashboard"'); ?>
+        <?php echo anchor('task/add', 'Add new task', 'class="btn btn_add"'); ?>
+        <?php echo anchor('transport/add', 'Create new transport', 'class="btn btn_transport"'); ?>
+    <?php } ?>
+    <?php echo anchor('login/logout', 'Logout', 'class="btn btn_logout"'); ?>
+    <div class="clear"></div>
+</div>
