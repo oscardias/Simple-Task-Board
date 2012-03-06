03234170d7f7a06b3b3cf5761517957fb7916193
Oscar Dias <oscardias@gmail.com>
2012-03-06 01:26:50 -0300

    Tasks in the Dasboard

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -17,23 +17,8 @@ class Dashboard extends CI_Controller {
         $data['projects'] = $this->project_model->get_user_related($this->session->userdata('user'));
         
         // Load tasks
-//        $this->load->model('task_model');
-//        $tasks = $this->task_model->get();
-//                
-//        foreach ($tasks as $task) {
-//            if ($task->status == 0) {
-//                $data['stories'][] = $task;
-//            } elseif ($task->status == 1) {
-//                $data['tasks'][] = $task;
-//            } elseif ($task->status == 2) {
-//                $data['tests'][] = $task;
-//            } elseif ($task->status == 3) {
-//                $data['done'][] = $task;
-//            }
-//        }
-
-        // Load text helper to be used in the view
-        $this->load->helper('text');
+        $this->load->model('task_model');
+        $tasks = $this->task_model->get_user_tasks($this->session->userdata('user'));
         
         // Load View
         $this->template->show('dashboard', $data);
