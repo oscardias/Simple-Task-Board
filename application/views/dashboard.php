28ddccac94ca05522c65f22dcf0cfa62421e6bd1
Oscar Dias <oscardias@gmail.com>
2012-03-06 01:27:49 -0300

    Dashboard view updates

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -4,9 +4,19 @@ $this->template->menu('dashboard');
 ?>
 
 <div id="container">
-    <?php if(isset($projects)) { ?>
-    <?php foreach ($projects as $project) { ?>
-        <?php echo $project->name; ?>
+    <?php if(isset($tasks)) { ?>
+    <div id="tasks">
+    <?php foreach ($tasks as $task) { ?>
+        #<?php echo $task->id; ?> <?php echo $task->title; ?>
     <?php } ?>
+    </div>
+    <?php } ?>
+    
+    <?php if(isset($projects)) { ?>
+    <div id="projects">
+    <?php foreach ($projects as $project) { ?>
+        <?php echo anchor('project/tasks/'.$project->id, $project->name); ?>
+    <?php } ?>
+    </div>
     <?php } ?>
 </div>
