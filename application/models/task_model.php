ca3cbdfb1976ceb829e3bba8070b82610a6f333c
Oscar Dias <oscardias@gmail.com>
2012-03-06 01:27:36 -0300

    Get user and project tasks

--- application/models/task_model.php
+++ application/models/task_model.php
@@ -24,6 +24,30 @@ class Task_model extends CI_Model {
         $this->db->order_by('priority', 'asc');
         $get = $this->db->get('task');
 
+        if ($id) return $get->row();
+        if($get->num_rows > 0) return $get->result();
+        return array();
+    }
+    
+    public function get_project_tasks($project)
+    {
+        if ($project) $this->db->where('project', $project);
+        $this->db->order_by('status', 'asc');
+        $this->db->order_by('priority', 'asc');
+        $get = $this->db->get('task');
+
+        if($get->num_rows > 0) return $get->result();
+        return array();
+    }
+    
+    public function get_user_tasks($user)
+    {
+        $this->db->from('task t');
+        $this->db->join('task_history h', 't.id = h.task');
+        $this->db->where('h.user', $user);
+        $this->db->order_by('t.status', 'asc');
+        $get = $this->db->get();
+
         if($get->num_rows > 0) return $get->result();
         return array();
     }
