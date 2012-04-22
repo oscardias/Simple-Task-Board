02c4aa035a39f6900044c5a740824a0b5024f170
Oscar Dias <oscardias@gmail.com>
2012-04-22 19:27:13 -0300

    User management

--- application/views/template/menu.php
+++ application/views/template/menu.php
@@ -3,12 +3,18 @@
     <?php if($view == 'dashboard') { ?>
         <!-- Dashboard menu -->
         <?php echo anchor('user', 'Edit Users', 'class="btn btn_users"'); ?>
+        <?php //echo anchor('project/add', 'Add new project', 'class="btn btn_add"'); ?>
+    <?php } elseif($view == 'users') { ?>
+        <!-- Users menu -->
+        <?php echo anchor('dashboard', 'Dashboard', 'class="btn btn_dashboard"'); ?>
+        <?php echo anchor('user/add', 'Add new user', 'class="btn btn_add"'); ?>
     <?php } elseif($view == 'task_board') { ?>
         <!-- Task board menu -->
         <?php echo anchor('dashboard', 'Dashboard', 'class="btn btn_dashboard"'); ?>
         <?php echo anchor('task/add', 'Add new task', 'class="btn btn_add"'); ?>
-        <?php echo anchor('transport/add', 'Create new transport', 'class="btn btn_transport"'); ?>
+        <?php //echo anchor('transport/add', 'Create new transport', 'class="btn btn_transport"'); ?>
     <?php } ?>
     <?php echo anchor('login/logout', 'Logout', 'class="btn btn_logout"'); ?>
     <div class="clear"></div>
 </div>
+<div id="page-title"><?php echo $page_title; ?></div>
