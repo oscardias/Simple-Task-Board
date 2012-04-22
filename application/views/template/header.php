8ff04a539808a6bf93b30f7503684238f28af92b
Oscar Dias <oscardias@gmail.com>
2012-04-22 19:28:19 -0300

    Javascripts inclusion

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -5,9 +5,12 @@
 	<title>Task Board<?php if (isset($page_title)) { echo ' | '.$page_title; } ?></title>
 
 	<link type="text/css" rel="stylesheet" href="<?php echo base_url(); ?>css/style.css" />
-    <!--[if IE]>
-    <link type="text/css" rel="stylesheet" href="<?php echo base_url(); ?>css/styleIE.css" />
-    <![endif]-->
-        
+        <!--[if IE]>
+        <link type="text/css" rel="stylesheet" href="<?php echo base_url(); ?>css/styleIE.css" />
+        <![endif]-->
+    
+        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.18/jquery-ui.min.js"></script>
+        <script type="text/javascript" src="<?php echo base_url(); ?>scripts/scripts.js"></script>
 </head>
 <body>
