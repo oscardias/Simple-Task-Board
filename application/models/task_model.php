a2b3d285bb8e3229e945516faffd185697e74bbe
Oscar Dias <oscardias@gmail.com>
2012-04-24 19:29:12 -0300

    Project/Task relationship update

--- application/models/task_model.php
+++ application/models/task_model.php
@@ -4,20 +4,32 @@ class Task_model extends CI_Model {
 
     public function create($data)
     {
+        $this->db->select_max('id');
+        $this->db->where('project', $data['project']);
+        $get = $this->db->get('task');
+        
+        if($get->num_rows > 0) {
+            $row = $get->row_array();
+            $data['id'] = $row['id'] + 1;
+        } else
+            $data['id'] = 1;
+        
         $insert = $this->db->insert('task', $data);
         return $insert;
     }
 
 
-    public function update($id, $data)
+    public function update($project, $id, $data)
     {
+        $this->db->where('project', $project);
         $this->db->where('id', $id);
         $update = $this->db->update('task', $data);
         return $update;
     }
 
-    public function get($id = false, $status = false)
+    public function get($project, $id = false, $status = false)
     {
+        $this->db->where('project', $project);
         if ($id) $this->db->where('id', $id);
         if ($status) $this->db->where('status', $status);
         $this->db->order_by('status', 'asc');
@@ -29,26 +41,14 @@ class Task_model extends CI_Model {
         return array();
     }
     
-    public function get_project_tasks($project)
-    {
-        if ($project) $this->db->where('project', $project);
-        $this->db->order_by('status', 'asc');
-        $this->db->order_by('priority', 'asc');
-        $get = $this->db->get('task');
-
-        if($get->num_rows > 0) return $get->result_array();
-        return array();
-    }
-    
     public function get_user_tasks($user)
     {
         $this->db->select('t.*');
         $this->db->distinct();
         $this->db->from('task t');
-        $this->db->join('task_history h', 't.id = h.task');
-        $this->db->where('h.user', $user);
+        $this->db->where('t.user', $user);
         $this->db->where('t.status !=', 3);
-        $this->db->order_by('t.status', 'asc');
+        $this->db->order_by('t.status', 'desc');
         $get = $this->db->get();
 
         if($get->num_rows > 0) return $get->result_array();
@@ -60,8 +60,7 @@ class Task_model extends CI_Model {
         $this->db->select('t.*');
         $this->db->distinct();
         $this->db->from('task t');
-        $this->db->join('task_history h', 't.id = h.task');
-        $this->db->where('h.user', $user);
+        $this->db->where('t.user', $user);
         $this->db->where('t.project', $project);
         $this->db->where('t.status !=', 3);
         $this->db->order_by('t.status', 'desc');
@@ -70,9 +69,23 @@ class Task_model extends CI_Model {
         if($get->num_rows > 0) return $get->result_array();
         return array();
     }
-
-    public function delete($id)
+    
+    public function get_related_users($project)
     {
+        $this->db->select('u.*, up.project');
+        
+        $this->db->from('user u');
+        $this->db->join('user_project up', 'up.user = u.id and up.project = '.$project, 'left');
+        $this->db->order_by('u.email', 'asc');
+        $get = $this->db->get();
+
+        if($get->num_rows > 0) return $get->result_array();
+        return array();
+    }
+
+    public function delete($project, $id)
+    {
+        $this->db->where('project', $project);
         $this->db->where('id', $id);
         $this->db->delete('task');
     }
