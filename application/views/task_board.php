b23f045c45c6e7d5f708d4b9dc40612e1ad8e405
Oscar Dias <oscardias@gmail.com>
2012-04-24 19:27:53 -0300

    Task board updates

--- application/views/task_board.php
+++ application/views/task_board.php
@@ -35,13 +35,14 @@ $this->template->menu('task_board');
                       $i = 0; ?>
                 <?php foreach ($stories as $task) {
                       $i++; ?>
-                <div id="task_<?php echo $task['id']; ?>" class="task" <?php if($i >= 3) { ?>title="<?php echo $task['text']; ?>"<?php } ?>>
+                <div id="task_<?php echo $task['id']; ?>" <?php if($i >= 3) { ?>title="<?php echo $task['description']; ?>"<?php } ?>
+                     class="task <?php echo ($task['user'] == $current_user)?'my-task':'project-task'; ?>">
                     <p class="task_id">#<?php echo $task['id']; ?></p>
-                    <p class="task_title"><?php echo anchor('task/edit/'.$task['id'], $task['title']); ?></p>
+                    <p class="task_title"><?php echo anchor('task/edit/'.$project.'/'.$task['id'], $task['title']); ?></p>
                     <?php if($i < 3) { ?>
-                    <p class="task_text"><?php echo word_limiter($task['text'], 30); ?></p>
+                    <p class="task_text"><?php echo word_limiter($task['description'], 30); ?></p>
                     <?php } ?>
-                    <p class="task_links"><?php echo anchor('task/move/'.$task['id'].'/1', 'Start &raquo;'); ?></p>
+                    <p class="task_links"><?php echo anchor('task/move/'.$project.'/'.$task['id'].'/1', 'Start &raquo;'); ?></p>
                 </div>
                 <?php } ?>
                 <?php } ?>
@@ -49,16 +50,16 @@ $this->template->menu('task_board');
             <td>
                 <?php if(isset($tasks)) { ?>
                 <?php foreach ($tasks as $task) { ?>
-                <div id="task_<?php echo $task['id']; ?>" class="task">
+                <div id="task_<?php echo $task['id']; ?>" class="task <?php echo ($task['user'] == $current_user)?'my-task':'project-task'; ?>">
                     <p class="task_id">#<?php echo $task['id']; ?></p>
-                    <p class="task_title"><?php echo anchor('task/edit/'.$task['id'], $task['title']); ?></p>
-                    <p class="task_text"><?php echo word_limiter($task['text'], 30); ?></p>
+                    <p class="task_title"><?php echo anchor('task/edit/'.$project.'/'.$task['id'], $task['title']); ?></p>
+                    <p class="task_text"><?php echo word_limiter($task['description'], 30); ?></p>
                     <p class="task_info"><?php
                         $files = explode(",",$task['files']);
                         $database = explode(",",$task['database']);
                         echo "Changes: ".(($task['files'])?count($files):0)." files, ".(($task['database'])?count($database):0)." tables/fields"; ?></p>
-                    <p class="task_links"><?php echo anchor('task/move/'.$task['id'].'/0', '&laquo; Back'); ?> |
-                        <?php echo anchor('task/move/'.$task['id'].'/2', 'Test &raquo;'); ?></p>
+                    <p class="task_links"><?php echo anchor('task/move/'.$project.'/'.$task['id'].'/0', '&laquo; Back'); ?> |
+                        <?php echo anchor('task/move/'.$project.'/'.$task['id'].'/2', 'Test &raquo;'); ?></p>
                 </div>
                 <?php } ?>
                 <?php } ?>
@@ -66,16 +67,16 @@ $this->template->menu('task_board');
             <td>
                 <?php if(isset($tests)) { ?>
                 <?php foreach ($tests as $task) { ?>
-                <div id="task_<?php echo $task['id']; ?>" class="task" title="<?php echo $task['text']; ?>">
+                <div id="task_<?php echo $task['id']; ?>" class="task <?php echo ($task['user'] == $current_user)?'my-task':'project-task'; ?>" title="<?php echo $task['description']; ?>">
                     <p class="task_id">#<?php echo $task['id']; ?></p>
-                    <p class="task_title"><?php echo anchor('task/edit/'.$task['id'], $task['title']); ?></p>
-                    <!--<p class="task_text"><?php echo word_limiter($task['text'], 30); ?></p>-->
+                    <p class="task_title"><?php echo anchor('task/edit/'.$project.'/'.$task['id'], $task['title']); ?></p>
+                    <!--<p class="task_text"><?php echo word_limiter($task['description'], 30); ?></p>-->
                     <p class="task_info"><?php
                         $files = explode(",",$task['files']);
                         $database = explode(",",$task['database']);
                         echo "Changes: ".(($task['files'])?count($files):0)." files, ".(($task['database'])?count($database):0)." tables/fields"; ?></p>
-                    <p class="task_links"><?php echo anchor('task/move/'.$task['id'].'/1', '&laquo; Back'); ?> |
-                        <?php echo anchor('task/move/'.$task['id'].'/3', 'Finish &raquo;'); ?></p>
+                    <p class="task_links"><?php echo anchor('task/move/'.$project.'/'.$task['id'].'/1', '&laquo; Back'); ?> |
+                        <?php echo anchor('task/move/'.$project.'/'.$task['id'].'/3', 'Finish &raquo;'); ?></p>
                 </div>
                 <?php } ?>
                 <?php } ?>
@@ -83,11 +84,11 @@ $this->template->menu('task_board');
             <td>
                 <?php if(isset($done)) { ?>
                 <?php foreach ($done as $task) { ?>
-                <div id="task_<?php echo $task['id']; ?>" class="task" title="<?php echo $task['text']; ?>">
+                <div id="task_<?php echo $task['id']; ?>" class="task <?php echo ($task['user'] == $current_user)?'my-task':'project-task'; ?>" title="<?php echo $task['description']; ?>">
                     <p class="task_id">#<?php echo $task['id']; ?></p>
-                    <p class="task_title"><?php echo anchor('task/edit/'.$task['id'], $task['title']); ?></p>
-                    <!--<p class="task_text"><?php echo word_limiter($task['text'], 30); ?></p>-->
-                    <p class="task_links"><?php echo anchor('task/move/'.$task['id'].'/2', '&laquo; Back'); ?></p>
+                    <p class="task_title"><?php echo anchor('task/edit/'.$project.'/'.$task['id'], $task['title']); ?></p>
+                    <!--<p class="task_text"><?php echo word_limiter($task['description'], 30); ?></p>-->
+                    <p class="task_links"><?php echo anchor('task/move/'.$project.'/'.$task['id'].'/2', '&laquo; Back'); ?></p>
                 </div>
                 <?php } ?>
                 <?php } ?>
