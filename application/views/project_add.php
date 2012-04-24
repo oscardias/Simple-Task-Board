0585b9191566b6851160ad4f926c8e286d16e04f
Oscar Dias <oscardias@gmail.com>
2012-04-24 14:59:45 -0300

    Create and edit projects

---
+++ application/views/project_add.php
@@ -0,0 +1,75 @@
+<?php
+// Load Menu
+$this->template->menu('projects');
+?>
+
+<div id="container">
+
+    <?php echo form_open('project/save'); ?>
+
+    <table>
+        <tr>
+            <td>
+                <?php echo form_label('Name', 'name'); ?>
+            </td>
+            <td>
+                <?php echo form_input('name', $name); ?>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <?php echo form_label('Description', 'description'); ?>
+            </td>
+            <td>
+                <?php
+                $data = array('name'        => 'description',
+                              'id'          => 'description',
+                              'value'       => $description,
+                              'rows'        => '6',
+                              'cols'        => '80');
+
+                echo form_textarea($data); ?>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <div class="project-users blue-gradient">
+                    Associated Users
+                    <span class="show-hide-event expand" target-id="associated-users"></span>
+                </div>
+            </td>
+        </tr>
+        <tbody id="associated-users">
+        <tr>
+            <td colspan="2">
+            <?php $counter = 0; ?>
+            <?php foreach ($users as $user) { ?>
+            <div class="half-width">
+                <label>
+                <?php echo form_checkbox('users[]', $user['id'], (isset($id) && $user['project'])?1:0); ?>
+                <?php echo $user['email']; ?></label>
+                <?php //echo form_label($user['email'], 'users[]'); ?>
+            </div>
+                <?php $counter++; ?>
+                <?php if($counter % 2 == 0) { ?>
+                <!--</tr><tr>-->
+                <?php } ?>
+            <?php } ?>
+            </td>
+        </tr>
+        </tbody>
+        <tr>
+                <td colspan="2">
+                    <?php if (isset($id)) echo form_hidden('id', $id); ?>
+
+                    <div class="form-save-buttons">
+                        <?php echo form_submit('save', 'Save', 'class="btn-blue"'); ?>
+                        <?php echo form_button('cancel', 'Cancel', 'class="btn-blue" onClick="history.go(-1)"');; ?>
+                    </div>
+                </td>
+        </tr>
+    </table>
+
+    <?php echo form_close(); ?>
+
+</div>
