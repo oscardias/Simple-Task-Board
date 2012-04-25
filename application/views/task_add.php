796ae7897f0e0c5f47be531e2df481a030ed4a32
Oscar Dias <oscardias@gmail.com>
2012-04-25 15:42:13 -0300

    New task fix

--- application/views/task_add.php
+++ application/views/task_add.php
@@ -96,8 +96,10 @@ $this->template->menu('return_to_tasks');
                         <?php echo form_submit('save', 'Save', 'class="btn-blue"'); ?>
                         <?php echo form_button('cancel', 'Cancel', 'class="btn-blue" onClick="history.go(-1)"');; ?>
                         <?php
-                        $remove_url = base_url().'task/remove/'.$project.'/'.$id;
-                        echo form_button('remove', 'Remove', 'class="btn-blue" id="remove-task" target-url="'.$remove_url.'"'); ?>
+                        if(isset($id)) {
+                            $remove_url = base_url().'task/remove/'.$project.'/'.$id;
+                            echo form_button('remove', 'Remove', 'class="btn-blue" id="remove-task" target-url="'.$remove_url.'"');
+                        } ?>
                     </div>
                 </td>
         </tr>
