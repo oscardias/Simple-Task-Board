d2424f0a59a2b6c75e7f1bb95620d837d1aaff9c
Oscar Dias <oscardias@gmail.com>
2012-06-26 16:17:00 -0300

    Task template update

--- application/views/template/single_task.php
+++ application/views/template/single_task.php
@@ -1,10 +1,26 @@
-<div id="task_<?php echo $task['id']; ?>" <?php if($i >= 3) { ?>title="<?php echo $task['description']; ?>"<?php } ?>
+<div id="task_<?php echo $task['id']; ?>" <?php if($task['status'] == 3) { ?>title="<?php echo $task['description']; ?>"<?php } ?>
         class="task <?php echo ($task['user'] == $current_user)?'my-task':'project-task'; ?>">
     <p class="task_id">#<?php echo $task['id']; ?></p>
     <p class="task_title"><?php echo anchor('task/edit/'.$project.'/'.$task['id'], $task['title']); ?></p>
     <p class="task_user">Assigned to: <?php echo $users[$task['user']]['email']; ?></p>
-    <?php if($i < 3 && $task['status'] < 3 && $task['status'] != 1) { ?>
+    <?php if($task['status'] < 3) { ?>
     <p class="task_text"><?php echo word_limiter($task['description'], 30); ?></p>
     <?php } ?>
+    <?php if($task['status'] == 0) { ?>
     <p class="task_links"><?php echo anchor('task/move/'.$project.'/'.$task['id'].'/1', 'Start &raquo;'); ?></p>
+    <?php } else if($task['status'] == 1) { ?>
+    <p class="task_links">
+        <?php echo anchor('task/move/'.$project.'/'.$task['id'].'/0', '&laquo; Back'); ?>
+        |
+        <?php echo anchor('task/move/'.$project.'/'.$task['id'].'/2', 'Test &raquo;'); ?>
+    </p>
+    <?php } else if($task['status'] == 2) { ?>
+    <p class="task_links">
+        <?php echo anchor('task/move/'.$project.'/'.$task['id'].'/0', '&laquo; Back'); ?>
+        |
+        <?php echo anchor('task/move/'.$project.'/'.$task['id'].'/2', 'Finish &raquo;'); ?>
+    </p>
+    <?php } else if($task['status'] == 3) { ?>
+    <p class="task_links"><?php echo anchor('task/move/'.$project.'/'.$task['id'].'/0', '&laquo; Back'); ?></p>
+    <?php } ?>
 </div>
