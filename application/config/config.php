0fc4ac53795c66092a4486fc1f7d23480094199f
Oscar Dias <oscardias@gmail.com>
2012-08-07 10:15:10 -0300

    Auto detect base_url

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,9 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/task_board/';
+$root = "http://".$_SERVER['HTTP_HOST'];
+$root .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
+$config['base_url']    = "$root";
 
 /*
 |--------------------------------------------------------------------------
