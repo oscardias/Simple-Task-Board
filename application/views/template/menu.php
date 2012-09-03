8a87df01567352c4e7644458586cb6954b18e792
Oscar Dias <oscardias@gmail.com>
2012-09-03 16:18:42 -0300

    Fix in project edit menu and add usercontrol library for user level verification

--- application/views/template/menu.php
+++ application/views/template/menu.php
@@ -2,31 +2,64 @@
 <div id="menu">
     <?php if($view == 'dashboard') { ?>
         <!-- Dashboard menu -->
-        <?php echo anchor('user', 'Edit Users', 'class="btn btn_users"'); ?>
-        <?php echo anchor('project/add', 'Add new project', 'class="btn btn_add"'); ?>
+        <?php
+        if($this->usercontrol->has_permission('user'))
+            echo anchor('user', 'Edit Users', 'class="btn btn_users"');
+        ?>
+        <?php
+        if($this->usercontrol->has_permission('project'))
+            echo anchor('project/add', 'Add new project', 'class="btn btn_add"');
+        ?>
     <?php } elseif($view == 'task_board') { ?>
         <!-- Task board menu -->
         <?php echo anchor('dashboard', 'Dashboard', 'class="btn btn_dashboard"'); ?>
-        <?php echo anchor('project/edit/'.$project, 'Edit project', 'class="btn btn_edit"'); ?>
-        <?php echo anchor('task/add/'.$project, 'Add new task', 'class="btn btn_add"'); ?>
-        <?php //echo anchor('transport/add', 'Create new transport', 'class="btn btn_transport"'); ?>
+        <?php
+        if($this->usercontrol->has_permission('project'))
+            echo anchor('project/edit/'.$project, 'Edit project', 'class="btn btn_edit"');
+        ?>
+        <?php
+        if($this->usercontrol->has_permission('task'))
+            echo anchor('task/add/'.$project, 'Add new task', 'class="btn btn_add"');
+        ?>
     <?php } elseif($view == 'task_view') { ?>
         <!-- Task View menu -->
         <?php echo anchor('dashboard', 'Dashboard', 'class="btn btn_dashboard"'); ?>
-        <?php echo anchor('project/tasks/'.$project, 'Task Board', 'class="btn btn_taskboard"'); ?>
-        <?php echo anchor('task/edit/'.$project.'/'.$task, 'Edit Task', 'class="btn btn_taskboard"'); ?>
+        <?php
+        if($this->usercontrol->has_permission('project', 'tasks'))
+            echo anchor('project/tasks/'.$project, 'Task Board', 'class="btn btn_taskboard"');
+        ?>
+        <?php
+        if($this->usercontrol->has_permission('task'))
+            echo anchor('task/edit/'.$project.'/'.$task, 'Edit Task', 'class="btn btn_edit"');
+        ?>
     <?php } elseif($view == 'task_edit') { ?>
         <!-- Task Edit menu -->
         <?php echo anchor('dashboard', 'Dashboard', 'class="btn btn_dashboard"'); ?>
-        <?php echo anchor('project/tasks/'.$project, 'Task Board', 'class="btn btn_taskboard"'); ?>
-        <?php echo (isset($task))?anchor('task/view/'.$project.'/'.$task, 'View Task', 'class="btn btn_taskboard"'):''; ?>
+        <?php
+        if($this->usercontrol->has_permission('project', 'task'))
+            echo anchor('project/tasks/'.$project, 'Task Board', 'class="btn btn_taskboard"');
+        ?>
+        <?php
+        if($this->usercontrol->has_permission('project', 'task'))
+            echo (isset($task))?anchor('task/view/'.$project.'/'.$task, 'View Task', 'class="btn btn_taskboard"'):'';
+        ?>
     <?php } elseif($view == 'projects') { ?>
         <!-- Projects menu -->
         <?php echo anchor('dashboard', 'Dashboard', 'class="btn btn_dashboard"'); ?>
+    <?php } elseif($view == 'project_edit') { ?>
+        <!-- Task Edit menu -->
+        <?php echo anchor('dashboard', 'Dashboard', 'class="btn btn_dashboard"'); ?>
+        <?php
+        if($this->usercontrol->has_permission('project', 'task'))
+            echo anchor('project/tasks/'.$project, 'Task Board', 'class="btn btn_taskboard"');
+        ?>
     <?php } elseif($view == 'users') { ?>
         <!-- Users menu -->
         <?php echo anchor('dashboard', 'Dashboard', 'class="btn btn_dashboard"'); ?>
-        <?php echo anchor('user/add', 'Add new user', 'class="btn btn_add"'); ?>
+        <?php
+        if($this->usercontrol->has_permission('user'))
+            echo anchor('user/add', 'Add new user', 'class="btn btn_add"');
+        ?>
     <?php } ?>
     <?php echo anchor('login/logout', 'Logout', 'class="btn btn_logout"'); ?>
     <div class="clear"></div>
