24d4322f4a5108d412fe7b3e729a3264380be2f5
Oscar Dias <oscardias@gmail.com>
2012-09-13 17:41:17 -0300

    Database updates for version 1.2 - new database structures for task and task_comments and new database task_history

--- application/models/database_model.php
+++ application/models/database_model.php
@@ -2,7 +2,7 @@
 
 class Database_model extends CI_Model {
 
-    private $latest_database_version = '1.1';
+    private $latest_database_version = '1.2';
     private $current_database_version = '1.0';
     
     public function is_up_to_date()
@@ -30,6 +30,8 @@ class Database_model extends CI_Model {
         
         // Execute 1.0 -> 1.1 database updates
         if($this->current_database_version == '1.0') {
+            
+            $this->db->trans_start();
             
             //DROP TABLE transport
             if($this->table_exists('transport'))
@@ -71,9 +73,243 @@ class Database_model extends CI_Model {
             
             // Create database version indicator
             $this->update_setting('database_version', '1.1');
+            $this->current_database_version = '1.1';
             
             // Create installation date
             $this->update_setting('stb_install_date', date("Y-m-d H:i:s"));
+            
+            $this->db->trans_complete();
+            if($this->db->trans_status() === FALSE)
+                return false;
+            
+        }
+        
+        // Execute 1.1 -> 1.2 database updates
+        if($this->current_database_version == '1.1') {
+            
+            $this->db->trans_start();
+            
+            // Create new task table
+            if($this->table_exists('task_tmp'))
+                $this->dbforge->drop_table('task_tmp');
+            
+            if(!$this->field_exists('task', 'task_id')) {
+                $fields = array(
+                    'task_id' => array(
+                        'type' => 'INT',
+                        'constraint' => 10, 
+                        'unsigned' => TRUE,
+                        'auto_increment' => TRUE
+                    ),
+                    'project_id' => array(
+                        'type' => 'INT',
+                        'constraint' => 10, 
+                        'unsigned' => TRUE,
+                        'null' => FALSE
+                     ),
+                    'parent_id' => array(
+                        'type' => 'INT',
+                        'constraint' => 10, 
+                        'unsigned' => TRUE,
+                        'null' => TRUE
+                    ),
+                    'user_id' => array(
+                        'type' => 'INT',
+                        'constraint' => 10, 
+                        'unsigned' => TRUE,
+                        'null' => FALSE
+                     ),
+                    'code' => array(
+                        'type' => 'INT',
+                        'constraint' => 10, 
+                        'unsigned' => TRUE,
+                        'null' => FALSE
+                     ),
+                    'status' => array(
+                        'type' => 'TINYINT',
+                        'constraint' => 4, 
+                        'unsigned' => TRUE,
+                        'null' => FALSE
+                     ),
+                    'title' => array(
+                        'type' => 'VARCHAR',
+                        'constraint' => '50',
+                        'null' => FALSE
+                     ),
+                    'priority' => array(
+                        'type' => 'TINYINT',
+                        'constraint' => 4, 
+                        'unsigned' => TRUE,
+                        'null' => FALSE
+                     ),
+                    'description' => array(
+                        'type' => 'TEXT',
+                        'null' => FALSE
+                     ),
+                    'files' => array(
+                        'type' => 'TEXT',
+                        'null' => FALSE
+                     ),
+                    'database' => array(
+                        'type' => 'TEXT',
+                        'null' => FALSE
+                     ),
+                    'date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP'
+                );
+                $this->dbforge->add_field($fields);
+                $this->dbforge->add_key('task_id', TRUE);
+                $this->dbforge->create_table('task_tmp', TRUE);
+
+                // Indexes
+                $this->db->query('ALTER TABLE  `task_tmp` ADD INDEX  `status` (  `project_id` ,  `status` )');
+                $this->db->query('ALTER TABLE  `task_tmp` ADD INDEX  `parent` (  `parent_id` )');
+                
+                // Copy all items
+                $task_array = $this->db->get('task')->result_array();
+                
+                foreach ($task_array as $value) {
+                    $this->db->insert('task_tmp', array(
+                        'project_id' => $value['project'],
+                        'parent_id' => ($value['parent'])?$value['parent']:NULL,
+                        'user_id' => $value['user'],
+                        'code' => $value['id'],
+                        'status' => $value['status'],
+                        'title' => $value['title'],
+                        'priority' => $value['priority'],
+                        'description' => $value['description'],
+                        'files' => $value['files'],
+                        'database' => $value['database'],
+                        'date_created' => $value['date_created']
+                    ));
+                }
+                
+                // Drop original task table
+                $this->dbforge->drop_table('task');
+                
+                // Rename new table
+                $this->dbforge->rename_table('task_tmp', 'task');
+                
+                // Foreign Keys
+                $this->db->query('ALTER TABLE  `task` ADD CONSTRAINT `task_stbfk_1` FOREIGN KEY (`project_id`) REFERENCES `project` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION');
+                $this->db->query('ALTER TABLE  `task` ADD CONSTRAINT `task_stbfk_2` FOREIGN KEY (`parent_id`) REFERENCES `task` (`task_id`) ON DELETE CASCADE ON UPDATE NO ACTION');
+            }
+            
+            // Create new task table
+            if($this->table_exists('task_comments_tmp'))
+                $this->dbforge->drop_table('task_comments_tmp');
+                
+            if(!$this->field_exists('task_comments', 'task_comments_id')) {
+                $fields = array(
+                    'task_comments_id' => array(
+                        'type' => 'INT',
+                        'constraint' => 10, 
+                        'unsigned' => TRUE,
+                        'auto_increment' => TRUE
+                    ),
+                    'task_id' => array(
+                        'type' => 'INT',
+                        'constraint' => 10, 
+                        'unsigned' => TRUE,
+                        'null' => FALSE
+                    ),
+                    'user_id' => array(
+                        'type' => 'INT',
+                        'constraint' => 10, 
+                        'unsigned' => TRUE,
+                        'null' => FALSE
+                     ),
+                    'comment' => array(
+                        'type' => 'TEXT',
+                        'null' => FALSE
+                     ),
+                    'date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP'
+                );
+                $this->dbforge->add_field($fields);
+                $this->dbforge->add_key('task_comments_id', TRUE);
+                $this->dbforge->create_table('task_comments_tmp', TRUE);
+
+                // Indexes
+                $this->db->query('ALTER TABLE  `task_comments_tmp` ADD INDEX  `task` (  `task_id` )');
+                
+                // Copy all items
+                $task_array = $this->db->get('task_comments')->result_array();
+                
+                foreach ($task_array as $value) {
+                    $this->db->insert('task_comments_tmp', array(
+                        'task_id' => $value['project'],
+                        'user_id' => $value['user'],
+                        'comment' => $value['comment'],
+                        'date_created' => $value['date_created']
+                    ));
+                }
+                
+                // Drop original task table
+                $this->dbforge->drop_table('task_comments');
+                
+                // Rename new table
+                $this->dbforge->rename_table('task_comments_tmp', 'task_comments');
+                
+                // Foreign Keys
+                $this->db->query('ALTER TABLE  `task_comments` ADD CONSTRAINT `task_comments_stbfk_1` FOREIGN KEY (`task_id`) REFERENCES `task` (`task_id`) ON DELETE CASCADE ON UPDATE NO ACTION');
+            }
+            
+            //CREATE TABLE task_history
+            if(!$this->table_exists('task_history')) {
+                $fields = array(
+                    'task_history_id' => array(
+                        'type' => 'INT',
+                        'constraint' => 10, 
+                        'unsigned' => TRUE,
+                        'auto_increment' => TRUE
+                    ),
+                    'task_id' => array(
+                        'type' => 'INT',
+                        'constraint' => 10, 
+                        'unsigned' => TRUE,
+                        'null' => FALSE
+                    ),
+                    'status' => array(
+                        'type' => 'TINYINT',
+                        'constraint' => 4, 
+                        'unsigned' => TRUE,
+                        'null' => FALSE
+                     ),
+                    'date_created' => array(
+                        'type' => 'DATETIME',
+                        'null' => FALSE
+                     ),
+                    'date_finished' => array(
+                        'type' => 'DATETIME',
+                        'null' => TRUE
+                     ),
+                    'duration' => array(
+                        'type' => 'INT',
+                        'constraint' => 10, 
+                        'unsigned' => TRUE,
+                        'null' => TRUE
+                     )
+                );
+                $this->dbforge->add_field($fields);
+                $this->dbforge->add_key('task_history_id', TRUE);
+                $this->dbforge->create_table('task_history', TRUE);
+
+                $this->db->query('CREATE INDEX `task` ON `task_history`(`task_id`, `status`)');
+                
+                // Foreign Keys
+                $this->db->query('ALTER TABLE  `task_history` ADD CONSTRAINT `task_history_stbfk_1` FOREIGN KEY (`task_id`) REFERENCES `task` (`task_id`) ON DELETE CASCADE ON UPDATE NO ACTION');
+            }
+            
+            // Create database version indicator
+            $this->update_setting('database_version', '1.2');
+            $this->current_database_version = '1.2';
+            
+            // Create installation date
+            $this->update_setting('stb_install_date', date("Y-m-d H:i:s"));
+            
+            $this->db->trans_complete();
+            if($this->db->trans_status() === FALSE)
+                return false;
+            
         }
         
         return true;
@@ -100,6 +336,36 @@ class Database_model extends CI_Model {
         if ($query->num_rows == 0) {
             return false;
         }
+        return true;
+    }
+    
+    public function index_exists($table_name, $index_name, $validate = array())
+    {
+        // Check if index exits
+        $query = $this->db->query('show index from ' . $table_name . ' where key_name like \'' . $index_name . '\'');
+        if ($query->num_rows == 0) {
+            return false;
+        }
+        
+        if($validate){
+            $array = $query->result_array();
+            foreach ($array as $key => $value) {
+                if($value['Column_name'] !== $validate[$key])
+                    return false;
+            }
+        }
+        
+        return true;
+    }
+    
+    public function foreign_exists($key_name)
+    {
+        // Check if foreign key exits
+        $query = $this->db->query('select constraint_name from information_schema.key_column_usage where constraint_name = \''.$key_name.'\'');
+        if ($query->num_rows == 0) {
+            return false;
+        }
+        
         return true;
     }
     
