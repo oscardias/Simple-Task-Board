780fd99d524a823a78ee5bee8524bf5d37e8602e
Oscar Dias <oscardias@gmail.com>
2012-09-13 17:37:14 -0300

    Update session definitions in config file - expiration removed

--- application/config/config.php
+++ application/config/config.php
@@ -246,10 +246,10 @@ $config['encryption_key'] = 'j4Hg4sptw39HTE26dOIncYT67dH6yfi2';
 | 'sess_time_to_update'		= how many seconds between CI refreshing Session Information
 |
 */
-$config['sess_cookie_name']		= 'stb_session';
-$config['sess_expiration']		= 7200;
-$config['sess_expire_on_close']	= FALSE;
-$config['sess_encrypt_cookie']	= FALSE;
+$config['sess_cookie_name']		= 'stbsession';
+$config['sess_expiration']		= 0;
+$config['sess_expire_on_close']	= TRUE;
+$config['sess_encrypt_cookie']	= TRUE;
 $config['sess_use_database']	= TRUE;
 $config['sess_table_name']		= 'ci_sessions';
 $config['sess_match_ip']		= FALSE;
