1521fd6db73d4da949e3195dfdc3a91a81141d32
Oscar Dias <oscardias@gmail.com>
2012-12-03 08:19:00 -0200

    User profiles - additional information for users

--- application/models/user_model.php
+++ application/models/user_model.php
@@ -4,6 +4,11 @@ class User_model extends CI_Model {
 
     private $salt = 'r4nd0m';
     
+    private $error = false;
+    
+    // User info
+    private $photo;
+    
     public $USER_LEVEL_ADMIN = 1;
     public $USER_LEVEL_PM = 2;
     public $USER_LEVEL_DEV = 3;
@@ -58,4 +63,119 @@ class User_model extends CI_Model {
         $this->db->delete('user');
     }
 
+    public function upload_photo()
+    {
+        $this->load->library('upload');
+
+        $config = array(
+            'allowed_types' => 'gif|png|jpg|jpeg',
+            'upload_path' => getcwd().'/upload/profile/'.$this->session->userdata('user'),
+            'max_size' => 2048,
+            'overwrite' => true
+        );
+        $this->upload->initialize($config);
+
+        // Create path
+        if(is_dir($config['upload_path'])) {
+            
+            $objects = scandir($config['upload_path']);
+            foreach ($objects as $object)
+            {
+                if($object != '.' AND $object != '..')
+                {
+                    unlink($config['upload_path'].'/'.$object);
+                }
+            }
+            
+        } else {
+            
+            if(!mkdir($config['upload_path'], 0755, true)){
+                $this->error = 'An error occurred while we uploaded your picture.';
+                return false;
+            }
+            
+        }
+
+        // Run the upload
+        if (!$this->upload->do_upload('photo')) {
+            // Problem in upload
+            $this->error = $this->upload->display_errors();
+            return false;
+        }
+
+        // Resize images
+        $upload_data = $this->upload->data();
+        if(!$this->user_model->prepare_image($upload_data)){
+            return false;
+        }
+
+        $this->photo = $upload_data['file_name'];
+        
+        return true;
+    }
+
+    public function prepare_image($data)
+    {
+        // Make it square - Crop
+        if($data['image_width'] > $data['image_height'])
+            $size = $data['image_height'];
+        else
+            $size = $data['image_width'];
+        
+        $config = array(
+            'source_image'   => $data['full_path'],
+            'maintain_ratio' => false,
+            'width'          => $size,
+            'height'         => $size
+        );
+        
+        $this->load->library('image_lib'); 
+
+        $this->image_lib->clear();
+        $this->image_lib->initialize($config); 
+        if(!$this->image_lib->crop()){
+            $this->error = $this->image_lib->display_errors();
+            return false;
+        }
+        
+        // Resize in three different sizes
+        $target = array(
+            0 => array('name' => 'large', 'width' => 128, 'height' => 128),
+            1 => array('name' => 'medium', 'width' => 64, 'height' => 64),
+            2 => array('name' => 'thumb', 'width' => 32, 'height' => 32)
+        );
+        
+        for($i = 0; $i < count($target); $i++) {
+            // Image library settings
+            // Resize
+            $config = array(
+                'source_image' => $data['full_path'],
+                'new_image'    => $data['file_path'].$target[$i]['name'].$data['file_name'],
+                'width'        => $target[$i]['width'],
+                'height'       => $target[$i]['height'],
+                'master_dim'   => (($data['image_width'] / $data['image_height']) >= ($target[$i]['width'] / $target[$i]['height']))?'height':'width'
+            );
+
+            $this->image_lib->clear();
+            $this->image_lib->initialize($config); 
+            if(!$this->image_lib->resize()){
+                $this->error = $this->image_lib->display_errors();
+                return false;
+            }
+            
+        }
+        
+        return true;
+    }
+    
+    function get_photo()
+    {
+        return $this->photo;
+    }
+    
+    function error_message()
+    {
+        return $this->error;
+    }
+    
 }
